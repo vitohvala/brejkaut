ae0b4ee25dc623f58a9e8cf7f950eb95e2faec14
vitohvala <hvalavitalisu@gmail.com>
2023-12-15 04:13:16 +0100

    uklonjene nepootrebne stvari

--- src/main.c
+++ src/main.c
@@ -1,18 +1,25 @@
 #include <SDL2/SDL.h>
-#include <math.h>
-#include <stdint.h>
-#include <stdio.h>
 
-#define PRINT_SDL_ERROR printf("SDL Error: %s\n", SDL_GetError())
-#define WINDOW_HEIGHT 600
-#define WINDOW_WEIGHT 800
-#define FPS 60
+#define PRINT_SDL_ERROR     printf("SDL Error: %s\n", SDL_GetError())
+#define WINDOW_HEIGHT       600
+#define WINDOW_WEIGHT       800
+#define FPS                 60
+#define BACKGROUND          0xFF2B353B
+#define BALL_COLOR          0xFFD24A42
+#define BRICKS_COLOR        0xFF6681AA
 
-typedef struct{
-    int x_pos;
-    int y_pos;
-    int weight, height;
-}Bricks;
+enum {
+    BLUE,
+    GREEN,
+    RED,
+    ALPHA,
+    TOTAL
+};
+
+typedef union {
+    int color;
+    uint8_t rgba[TOTAL];
+}Color;
 
 typedef struct{
     float cx;
@@ -21,7 +28,6 @@ typedef struct{
     float dx;
     float dy;
 }Ball;
-
 
 /*TODO: FPS, draw better circle*/
 
@@ -38,14 +44,14 @@ void DrawCircle( SDL_Renderer *renderer, Ball ball) {
     float error = (tx - diameter);
 
     while( x >= y ){
-        points[drawCount+0].x =  ball.cx + x;
-        points[drawCount+1].x =  ball.cx + x;
-        points[drawCount+2].x =  ball.cx - x;
-        points[drawCount+3].x =  ball.cx - x;
-        points[drawCount+4].x =  ball.cx + y;
-        points[drawCount+5].x =  ball.cx + y;
-        points[drawCount+6].x =  ball.cx - y;
-        points[drawCount+7].x =  ball.cx - y;
+        points[drawCount+0].x = ball.cx + x;
+        points[drawCount+1].x = ball.cx + x;
+        points[drawCount+2].x = ball.cx - x;
+        points[drawCount+3].x = ball.cx - x;
+        points[drawCount+4].x = ball.cx + y;
+        points[drawCount+5].x = ball.cx + y;
+        points[drawCount+6].x = ball.cx - y;
+        points[drawCount+7].x = ball.cx - y;
 
         points[drawCount+0].y = ball.cy - y;
         points[drawCount+1].y = ball.cy + y;
@@ -77,26 +83,30 @@ void DrawCircle( SDL_Renderer *renderer, Ball ball) {
     SDL_RenderDrawLinesF(renderer, points, drawCount);
 }
 
-void rend(SDL_Renderer *renderer, Ball *ball, Bricks *bar, Bricks *blocks, int size){
-    SDL_SetRenderDrawColor(renderer, 43, 53, 59, 255);
-    
+void set_color(SDL_Renderer *renderer, const int color){
+    Color c;
+    c.color = color;
+    SDL_SetRenderDrawColor(renderer, c.rgba[RED], c.rgba[GREEN], c.rgba[BLUE], c.rgba[ALPHA]);
+}
+
+void render(SDL_Renderer *renderer, Ball *ball, SDL_Rect *bar, SDL_Rect *blocks, int size){
+    set_color(renderer, BACKGROUND);
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 210, 74, 66, 255);
+
+    set_color(renderer, BALL_COLOR);
     DrawCircle(renderer, *ball);
 
-    SDL_SetRenderDrawColor(renderer, 102, 129, 170, 255);
-    SDL_Rect bar_rect = {bar->x_pos, bar->y_pos, bar->weight, bar->height};
-    SDL_RenderFillRect(renderer, &bar_rect);
+    set_color(renderer, BRICKS_COLOR);
+    SDL_RenderFillRect(renderer, bar);
 
     for(int i = 0; i < size; i++){
-        SDL_Rect block = {blocks[i].x_pos, blocks[i].y_pos, blocks[i].weight, blocks[i].height};
-        SDL_RenderFillRect(renderer, &block);
+        SDL_RenderFillRect(renderer, &blocks[i]);
     }
 
     SDL_RenderPresent(renderer);
 }
 
-void inputCntrl(SDL_Window *window, uint8_t *end, Bricks *bar){
+void control_input(SDL_Window *window, uint8_t *end, SDL_Rect *bar){
     SDL_Event event;
 
     while (SDL_PollEvent(&event)) {
@@ -118,19 +128,14 @@ void inputCntrl(SDL_Window *window, uint8_t *end, Bricks *bar){
         }
     }
     const uint8_t *keyboard = SDL_GetKeyboardState(NULL);
-    if((keyboard[SDL_SCANCODE_A] || keyboard[SDL_SCANCODE_LEFT]) && bar->x_pos > 0) bar->x_pos -= 4;
-    else if((keyboard[SDL_SCANCODE_D] || keyboard[SDL_SCANCODE_RIGHT]) && bar->x_pos + bar->weight < WINDOW_WEIGHT) bar->x_pos += 4;
+    if((keyboard[SDL_SCANCODE_A] || keyboard[SDL_SCANCODE_LEFT]) && bar->x > 0) bar->x -= 4;
+    else if((keyboard[SDL_SCANCODE_D] || keyboard[SDL_SCANCODE_RIGHT]) && bar->x + bar->w < WINDOW_WEIGHT) bar->x += 4;
 }
 
-uint8_t collision(SDL_Rect rectball, SDL_Rect rectbar, Bricks *blocks, int *size){
+uint8_t collision(SDL_Rect rectball, SDL_Rect rectbar, SDL_Rect *blocks, int *size){
     if(SDL_HasIntersection(&rectbar, &rectball)) return 1;
-    
     for(int i = 0; i < *size; i++){
-        rectbar.x = blocks[i].x_pos;
-        rectbar.y = blocks[i].y_pos;
-        rectbar.w = blocks[i].weight;
-        rectbar.h = blocks[i].height;
-        if(SDL_HasIntersection(&rectball, &rectbar)){
+        if(SDL_HasIntersection(&rectball, &blocks[i])){
             (*size)--;
             blocks[i] = blocks[(*size)];
             return 1;
@@ -139,13 +144,11 @@ uint8_t collision(SDL_Rect rectball, SDL_Rect rectbar, Bricks *blocks, int *size
     return 0;
 }
 
-uint8_t update(Ball *ball, Bricks bar, Bricks *blocks, int *size){
+uint8_t update(Ball *ball, SDL_Rect bar, SDL_Rect *blocks, int *size){
     uint8_t flag = 1;
     float nx = ball->cx + ball->dx;
     SDL_Rect rectball = {(int)(nx - ball->radius), (int)(ball->cy - ball->radius), (int)ball->radius*2, (int)ball->radius*2};
-    SDL_Rect rectbar = {bar.x_pos, bar.y_pos, bar.weight, bar.height};
-    
-    if(ball->cx > WINDOW_WEIGHT - ball->radius || ball->cx - ball->radius  < 0 || collision(rectball, rectbar, blocks, size)){
+    if(ball->cx > WINDOW_WEIGHT - ball->radius || ball->cx - ball->radius  < 0 || collision(rectball, bar, blocks, size)){
         ball->dx *= -1;
         nx = ball->cx + ball->dx;
         flag = 2;
@@ -155,27 +158,27 @@ uint8_t update(Ball *ball, Bricks bar, Bricks *blocks, int *size){
     float ny = ball->cy + ball->dy;
     rectball.y = ny - ball->radius;
 
-    if(ball->cy - ball->radius < 0 || collision(rectball, rectbar, blocks, size)) {
+    if(ball->cy - ball->radius < 0 || collision(rectball, bar, blocks, size)) {
         ball->dy *= -1;
         ny = ball->cy + ball->dy;
         flag = 2;
     }
-    if(ball->cy - ball->radius > bar.y_pos + bar.height) flag = 0;
+    if(ball->cy - ball->radius > bar.y + bar.h) flag = 0;
 
     ball->cx = nx;
     ball->cy = ny;
     return flag;
 }
 
-void init_blocks(Bricks *blocks, int bricks_size){
+void init_blocks(SDL_Rect *blocks, int bricks_size){
     int tempx = 0;
     int tempy = 30;
 
     for(int i = 0; i < bricks_size; i++){
-        blocks[i].x_pos = 4 + tempx;
-        blocks[i].y_pos = tempy;
-        blocks[i].weight = (WINDOW_WEIGHT / 8) - 8;
-        blocks[i].height = 25;
+        blocks[i].x = 4 + tempx;
+        blocks[i].y = tempy;
+        blocks[i].w = (WINDOW_WEIGHT / 8) - 8;
+        blocks[i].h = 25;
         tempx += (WINDOW_WEIGHT / 8);
         if(i % 8 == 7){
             tempx = 0;
@@ -188,14 +191,14 @@ void init_blocks(Bricks *blocks, int bricks_size){
 
 int main(){
     SDL_Window *window;
-    int retval = 0;
     SDL_Renderer *renderer;
+    int retval = 0;
 
     Ball ball = {((float)WINDOW_WEIGHT / 2) - 13, ((float)WINDOW_HEIGHT / 2) - 13, 13, 2.5, 2.5};
-    Bricks bar = {(WINDOW_WEIGHT / 2) - 50, WINDOW_HEIGHT - 25, 100, 15};
+    SDL_Rect bar = {(WINDOW_WEIGHT / 2) - 50, WINDOW_HEIGHT - 25, 100, 15};
 
     int bricks_size = 8 * 6;
-    Bricks blocks[bricks_size];
+    SDL_Rect blocks[bricks_size];
     init_blocks(blocks, bricks_size);
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -204,7 +207,7 @@ int main(){
     }
 
     window = SDL_CreateWindow("Breakout", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                               WINDOW_WEIGHT, WINDOW_HEIGHT, 0);
+            WINDOW_WEIGHT, WINDOW_HEIGHT, 0);
     if(window == NULL){
         PRINT_SDL_ERROR;
         retval = -1;
@@ -222,15 +225,12 @@ int main(){
 
     while (end && bricks_size){
         end = update(&ball, bar, blocks, &bricks_size);
-        inputCntrl(window, &end, &bar);
-        rend(renderer, &ball, &bar, blocks, bricks_size);
+        control_input(window, &end, &bar);
+        render(renderer, &ball, &bar, blocks, bricks_size);
     }
-    
     SDL_DestroyRenderer(renderer);
-REN_ERR:    
-    SDL_DestroyWindow(window);
-WIN_ERR:
-    SDL_Quit();
+REN_ERR: SDL_DestroyWindow(window);
+WIN_ERR: SDL_Quit();
 
-    return retval;
+         return retval;
 }
